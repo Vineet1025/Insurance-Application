cc53e4714d721f3abe92140dc6de9ec9bc5555ac
vineet <jvineet1025@gmail.com>
2024-07-05 00:28:03 +0530

    add

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,11 @@
+
+Š
+Znotification/src/main/java/com/impetus/Insurance/notification/NotificationApplication.java,4\d\4df7fdc49ed6f517063e2d08f00d30a82155ef58
+‹
+[InsuranceApplication/src/main/java/com/impetus/insurance/app/constants/AccessConstants.java,d\4\d42b80716ec4aa831a0ad7fcf92786f8d8fc62f0
+…
+UInsuranceApplication/src/main/java/com/impetus/insurance/InsuranceAppApplication.java,d\8\d8769fa6ba5bbe60fd6765ecd356a17961775d0b
+Œ
+\InsuranceApplication/src/main/java/com/impetus/insurance/app/config/security/JwtService.java,4\a\4ade18fb9f9aab9818f75b537d65483ec230233c
+š
+jInsuranceApplication/src/main/java/com/impetus/insurance/app/config/security/ApplicationConfiguration.java,5\4\5455cd147ac47bc7e4e2807912b9ab5a92e74e90
---
+++ InsuranceApplication/src/main/java/com/java/insurance/app/controllers/AuthController.java
@@ -0,0 +1,42 @@
+package com.java.insurance.app.controllers;
+
+import com.java.insurance.app.services.AuthenticationService;
+import com.java.insurance.app.models.User;
+import com.java.insurance.app.payload.AuthenticationRequest;
+import com.java.insurance.app.payload.AuthenticationResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.java.insurance.app.constants.AuthConstants.LOGIN;
+import static com.java.insurance.app.constants.AuthConstants.REGISTER;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthenticationService authenticationService;
+
+    /**
+     * Registers a new user.
+     *
+     * @param user The user object containing registration details.
+     * @return ResponseEntity containing the authentication response.
+     */
+    @PostMapping(REGISTER)
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody User user) {
+        return ResponseEntity.ok(authenticationService.register(user));
+    }
+
+    /**
+     * Logs in a user.
+     *
+     * @param authenticationRequest The authentication request containing user credentials.
+     * @return ResponseEntity containing the authentication response.
+     */
+    @PostMapping(LOGIN)
+    public ResponseEntity<AuthenticationResponse> login(@RequestBody AuthenticationRequest authenticationRequest) {
+        return ResponseEntity.ok(authenticationService.login(authenticationRequest));
+    }
+}
